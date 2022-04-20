9412d2b65d98c33e5a7996854911417918f80ef7
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2022-04-20 15:09:02 +0300

    Dairenin Alanını ve Çevresini Hesaplayan Program

--- src/DaireninAlaniniveCevresiniHesaplayanProgram.java
+++ src/DaireninAlaniniveCevresiniHesaplayanProgram.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
+
+/**
+ * @author ANIL CAN ÖZDEMİR
+ * Dairenin Alanını ve Çevresini Hesaplayan Program
+ * Java ile yarı çapını kullanıcıdan aldığınız dairenin alanını ve çevresini hesaplayan programı yazın.
+ *
+ * Alan Formülü : π * r * r;
+ *
+ * Çevre Formülü : 2 * π * r;
+ */
+
 public class DaireninAlaniniveCevresiniHesaplayanProgram {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Yarıçapı giriniz");
+        double r=input.nextDouble();
+        double alan = Math.PI * r * r;
+        double cevre = Math.PI*2*r;
+        System.out.println("Alan"+alan);
+        System.out.println("Cevre:"+cevre);
+    }
 }
